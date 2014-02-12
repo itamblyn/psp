bf3bf1b944cbac0f8d3b86c0ca54778113a3ab04
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2014-02-12 17:49:02 -0500

    fixed issue with pswatch, it assumed old tail behaviour

--- h_hard_IT.ini/run.sh
+++ h_hard_IT.ini/run.sh
@@ -4,6 +4,6 @@ mv h_hard_IT.ini ~/
 rm h* x*
 mv ~/h_hard_IT.ini .
 psgen -o h_hard_IT h_hard_IT.ini
-fhi2cpmd h_hard_IT.dat h_hard_IT.cpi h_hard_IT.cpmd.psp
+~/git/psp/fhi2cpmd/fhi2cpmd h_hard_IT.dat h_hard_IT.cpi h_hard_IT.cpmd.psp
 pswatch -i h_hard_IT h_hard_IT.ini -l 0
 
