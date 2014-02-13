165548799e923c4cda871807d2aa3f22bf2a39fe
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2014-02-12 19:35:59 -0500

    making abinit forms for boron and carbon, testing vca mixing

--- 05-B:h.ini/run.sh
+++ 05-B:h.ini/run.sh
@@ -7,4 +7,4 @@ rm 05* x*
 mv ~/"$name".ini .
 ~/git/psp/fhi98PP/bin/Tools/psgen -o "$name" "$name".ini
 ~/git/psp/fhi2cpmd/fhi2cpmd "$name".dat "$name".cpi "$name".psp
-~/git/psp/fhi98PP/bin/Tools/pswatch -i "$name" "$name".ini -l 1
+~/git/psp/fhi98PP/bin/Tools/pswatch -i "$name" "$name".ini -l 2
--- 06-C:tm.ini/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mv 06\-C:tm.ini ~/
-rm 06* x*
-mv ~/06\-C:tm.ini .
-psgen -o 06\-C:tm 06\-C:tm.ini
-~/git/psp/fhi2cpmd/fhi2cpmd 06\-C:tm.dat 06\-C:tm.cpi 06\-C:tm.psp
-pswatch -i 06\-C:tm 06\-C:tm.ini -l 1
